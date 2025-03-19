bbade5fe71e64a63b1bc2b87e916ce94b32bab0b
BoKyung Ryu <dbqhrud5789@naver.com>
2025-03-19 17:13:18 +0900

    Update st_c3.cpp

--- Day01/st_c3.cpp
+++ Day01/st_c3.cpp
@@ -1,28 +1,28 @@
-/*
-	C++ class
-*/
-
-#include<iostream>
-
-using namespace std;
-
-class Human {
-public:
-	char name[20];
-	int age;
-	char job[10];
-
-	Human() {}	// 디폴트 생성자, 리턴값이 없음, 클래스이름이랑 같은 이름 
-
-	void showHuman() {
-		cout << "name : " << name << ", age : " << age << ", job : " << job << endl;
-	}
-};
-
-int main() {
-
-	Human h = { "김철수", 30,"백수" };
-	h.showHuman();
-
-	return 0;
-}
+///*
+//	C++ class
+//*/
+//
+//#include<iostream>
+//
+//using namespace std;
+//
+//class Human {
+//public:
+//	char name[20];
+//	int age;
+//	char job[10];
+//
+//	Human() {}	// 디폴트 생성자, 리턴값이 없음, 클래스이름이랑 같은 이름 
+//
+//	void showHuman() {
+//		cout << "name : " << name << ", age : " << age << ", job : " << job << endl;
+//	}
+//};
+//
+//int main() {
+//
+//	Human h = { "김철수", 30,"백수" };
+//	h.showHuman();
+//
+//	return 0;
+//}
